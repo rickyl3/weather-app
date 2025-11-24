88e9696273d7d86bdfe2069aab0dacfffeb73718
Ricky Leung <rl9480@rit.edu>
2025-11-23 23:48:31 -0500

    Update location title to show what each part of title is

--- src/App.js
+++ src/App.js
@@ -131,10 +131,20 @@ function App() {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
-                        <h2>
-                            {weatherData.location.name}
-                            {weatherData.location.country && `, ${weatherData.location.country}`}
-                        </h2>
+                        <div>
+                            <h2>
+                                {weatherData.location.name}
+                                {weatherData.location.region && `, ${weatherData.location.region}`}
+                                {weatherData.location.country && `, ${weatherData.location.country}`}
+                            </h2>
+                            <p className="App-location-labels">
+                                <em>
+                                    name
+                                    {weatherData.location.region && ', region'}
+                                    {weatherData.location.country && ', country'}
+                                </em>
+                            </p>
+                        </div>
                     </div>
 
                     <div className="App-weather-grid">
